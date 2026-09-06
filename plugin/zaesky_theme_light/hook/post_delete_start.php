58391e511fa90cb3f87fb8a322b9e802ff7d9c18
xiuno_xw <xiuno_xw@users.noreply.github.com>
2026-09-06 19:23:37 +0800

    v1.7.0: 原版主题新增左侧版块展示栏，移除轻鸿插件

--- plugin/zaesky_theme_light/hook/post_delete_start.php
+++
@@ -1,1 +0,0 @@
-if($gid > 5 && isset($light_config['thread_user_del']) && $light_config['thread_user_del'] == 1) message(-1, lang('no_permission_delete'));
--- plugin/zaesky_theme_light/hook/post_update_get_start.php
+++
@@ -1,1 +0,0 @@
-if($gid > 5 && isset($light_config['thread_user_upd']) && $light_config['thread_user_upd'] == 1) message(-1, lang('no_permission_update'));
